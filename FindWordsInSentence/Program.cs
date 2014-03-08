c963bcaaf01c06ba2d47521867960a9a13cde99b
VegaFromLyra <asha.ab@gmail.com>
2014-03-07 16:18:54 -0800

    Given a sentence without spaces, find valid words in it

---
+++ FindWordsInSentence/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Given a sentence without spaces and a 
+// dictionary of valid words,
+// print the valid words in the sentence
+namespace FindWordsInSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> dictionary = new HashSet<string>();
+            dictionary.Add("the");
+            dictionary.Add("hen");
+
+            string sentenceWithoutSpaces = "thehen";
+
+            PrintWords(sentenceWithoutSpaces, dictionary);
+        }
+
+        static void PrintWords(string sentence, HashSet<string> dictionary)
+        {
+            int start = 0;
+            int end = 0;
+
+            while (start < sentence.Length && end < sentence.Length)
+            {
+                string word = sentence.Substring(start, end - start + 1);
+                if (dictionary.Contains(word))
+                {
+                    Console.WriteLine(word);
+                    end++;
+                    start = end;
+                }
+
+                end++;
+            }
+        }
+    }
+}
